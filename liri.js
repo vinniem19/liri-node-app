1e6a495f5587ca1412a828008a1c836a0b3e9507
unknown <cuzvin2@gmail.com>
2019-05-01 18:07:31 -0400

    everything functional

--- liri.js
+++ liri.js
@@ -5,31 +5,13 @@ let spotify = new Spotify(keys.spotify);
 let fs = require("fs");
 let axios = require("axios");
 let moment = require("moment");
-// let random = require("random.txt");
-// Make it so liri.js can take in one of the following commands:
 
-let command = process.argv[2];
 
-// I could make each "command" a function
-// and switch to those functions based on the command
-switch (command) {
-    case "concert-this":
-        concertThis();
-        break;
-    case "spotify-this-song":
-        spotifyThis();
-        break;
-    case "movie-this":
-        movieThis();
-        break;
-    case "do-what-it-says":
-        doWhatItSays();
-}
 
 function concertThis() {
     // node liri.js concert-this <artist/band name here>
     // We want to show
-    let artist = "";
+     let artist = "";
     for (let i = 3; i < process.argv.length; i++) {
         if (i > 3 && i < process.argv.length) {
             artist = artist + "+" + process.argv[i];
@@ -58,34 +40,31 @@ function concertThis() {
             let formattedDate = moment(eventDate).format("MM/DD/YYYY");
             console.log("Next Concert Date: " + formattedDate);
         })
-                 .catch(function(err) {
-                  console.error('Error occurred: ' + err); 
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
         });
 }
 
 
 //    * `spotify-this-song`
-function spotifyThis() {
+function spotifyThis(songName) {
 
-    // let spotify = new Spotify({
-    //     id: keys["spotify"].id,
-    //     secret: keys["spotify"].secret
-    // })
-
-    let songName = "";
-    if (process.argv[3] == null) {
+    //   * If no song is provided then your program will default to "The Sign" by Ace of Base.
+    if (process.argv[3] == null && (process.argv[2] !== "do-what-it-says")) {
         songName = "The+Sign+Ace+of+Base";
-    }
-    for (let i = 3; i < process.argv.length; i++) {
-        
-        if (i > 3 && i < process.argv.length) {
-            songName = songName + "+" + process.argv[i];
-        } else {
-            songName += process.argv[i];
+    } else if (process.argv[2] !== "do-what-it-says") {
+
+
+        for (let i = 3; i < process.argv.length; i++) {
+
+            if (i > 3 && i < process.argv.length) {
+                songName = songName + "+" + process.argv[i];
+            } 
         }
     }
+
+    console.log(songName);
     let queryURL = "https://api.spotify.com/v1/search?q=track:" + songName + "&type=track&limit=10";
-    // console.log(queryURL);
 
     spotify
         .request(queryURL)
@@ -104,16 +83,6 @@ function spotifyThis() {
         .catch(function (err) {
             console.error('Error occurred: ' + err);
         });
-
-    //   * If no song is provided then your program will default to "The Sign" by Ace of Base.
-
-    // console.log(theSignData)
-    // console.log(theSignData.tracks.items[i].artists[0].name);
-    // console.log(theSignData.tracks.items[i].name);
-    // console.log(theSignData.tracks.items[i].preview_url);
-    // console.log(theSignData.tracks.items[i].album.name);
-
-
 }
 
 
@@ -126,8 +95,8 @@ function movieThis() {
         movieName = "Mr+Nobody";
     }
     for (let i = 3; i < process.argv.length; i++) {
-         
-           if (i > 3 && i < process.argv.length) {
+
+        if (i > 3 && i < process.argv.length) {
             movieName = movieName + "+" + process.argv[i];
             // var movieName = "Mr. Nobody";
         } else {
@@ -141,13 +110,13 @@ function movieThis() {
     // using axios
     axios.get(queryUrl).then(
         function (response) {
-          // console.log(response.data);
+            // console.log(response.data);
             // Mr. Nobody will be a default movie for data output if no movie title input by user
 
             //      * Title of the movie.
-             console.log("Movie title: " + response.data.Title)
+            console.log("Movie title: " + response.data.Title)
             //      * Year the movie came out.
-             console.log("Release Year: " + response.data.Year);
+            console.log("Release Year: " + response.data.Year);
 
             //      * IMDB Rating of the movie.
             console.log("IMDB Rating: " + response.data.imdbRating);
@@ -163,20 +132,42 @@ function movieThis() {
             console.log("Actors: " + response.data.Actors);
         }
     )
-    .catch(function (err) {
-        console.error('Error occurred: ' + err);
-});
+        .catch(function (err) {
+            console.error('Error occurred: ' + err);
+        });
 }
 //    * `do-what-it-says`
-// This is using a require of the random.txt file
-    function doWhatItSays() {
-        fs.readFile("random.txt", "utf8", function(err, data) {
-            if (err) {
-                return console.log(err)
-            }
-            let output = data.split(",");
-            for (let i=0; i < output.length; i++) {
-                console.log(output[i]);
-            }
-        });
-    };
+// This is using a readFile of the random.txt file
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err)
+        }
+        let output = data.split(",");
+
+        console.log(output);
+
+        commandCheck(output[0], output[1]);
+
+    });
+};
+
+let commandCheck = function (caseOutput, functionData) {
+    switch (caseOutput) {
+        case "concert-this":
+            concertThis(functionData);
+            break;
+        case "spotify-this-song":
+            spotifyThis(functionData);
+            break;
+        case "movie-this":
+            movieThis(functionData);
+            break;
+        case "do-what-it-says":
+            doWhatItSays(functionData);
+    }
+}
+function doCommand(argOne, argTwo) {
+    commandCheck(argOne, argTwo);
+}
+doCommand(process.argv[2], process.argv[3]);
